dcb58590ac1ff239dde9cbf6d59c1cb22e71cc00
Srinivas Hegde M <hegdesrinivasm@gmail.com>
2026-08-09 23:33:30 +0530

    feat: add nvim leader keymaps and editor shortcuts

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -34,3 +34,41 @@ vim.opt.timeoutlen = 300
 -- ==========================================================
 vim.cmd.colorscheme("habamax")
 -- Alternatives: vim.cmd.colorscheme("gruvbox") or ("desert")
+
+-- ==========================================================
+-- 3. Keymaps
+-- ==========================================================
+vim.g.mapleader = " "
+vim.g.localleader = " "
+vim.g.netrw_banner = 0
+
+-- Save
+vim.keymap.set("n", "<Space>s", ":write<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<C-s>", ":write<CR>", { desc = "Save file" })
+vim.keymap.set("i", "<C-s>", "<Esc>:write<CR>", { desc = "Save file" })
+
+-- Quit
+vim.keymap.set("n", "<Space>q", ":quit<CR>", { desc = "Quit nvim" })
+
+-- File explorer (netrw)
+vim.keymap.set("n", "<Space>e", ":Explore<CR>", { desc = "Open file explorer" })
+
+-- Toggle relative line numbers
+vim.keymap.set("n", "<Space>n", function()
+  vim.opt.relativenumber = not vim.opt.relativenumber:get()
+end, { desc = "Toggle relative line numbers" })
+
+-- Window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to window below" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to window above" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
+-- Window resize
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+
+-- Terminal
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
