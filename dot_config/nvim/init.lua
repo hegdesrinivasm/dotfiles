2b1b21ae7d268335b97eeb54f480fd9c7d72daf4
Srinivas Hegde M <hegdesrinivasm@gmail.com>
2026-08-11 20:26:08 +0530

    feat: spiderman theming for ghostty and nvim (#19)

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -45,16 +45,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = { style = "night" },
-  },
-})
+require("lazy").setup({})
 
-vim.cmd.colorscheme("tokyonight")
+vim.cmd.colorscheme("spiderman")
 
 -- ==========================================================
 -- 3. Keymaps
