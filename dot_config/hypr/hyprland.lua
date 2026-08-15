29ef07f34c51e6354e2700bb730d426130603c9f
Srinivas Hegde M <hegdesrinivasm@gmail.com>
2026-08-15 14:15:35 +0530

    feat: audio source switcher for waybar and spiderman active border

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -118,7 +118,7 @@ hl.config({
         border_size = 2,
 
         col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
+            active_border   = { colors = {"rgba(e60813ff)", "rgba(2c3fffff)"}, angle = 45 },
             inactive_border = "rgba(595959aa)",
         },
 
